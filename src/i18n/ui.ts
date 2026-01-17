1e81c7d744cfd1fb60737b364ef6587a39d3092c
David Ye <36756519+ysd1123@users.noreply.github.com>
2026-01-17 14:33:01 +0800

    fix: update language type definitions across multiple files

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -10,19 +10,19 @@ interface Translation {
   toc: string
 }
 
-export const ui: Record<Language, Translation> = {
-  'en': {
+export const ui: Partial<Record<Language, Translation>> = {
+  en: {
     title: 'CodeRiff',
-    subtitle: "@Coconut Jelly's Personal Blog",
-    description: "David Ye's personal tech blog, which may also include some life essays. Powered by Astro theme Retypeset and GitHub Pages.",
+    subtitle: '@Coconut Jelly\'s Personal Blog',
+    description: 'David Ye\'s personal tech blog, which may also include some life essays. Powered by Astro theme Retypeset and GitHub Pages.',
     posts: 'Posts',
     tags: 'Tags',
     about: 'About',
     toc: 'Table of Contents',
   },
-  'zh': {
+  zh: {
     title: '码上观澜',
-    subtitle: "椰冻的个人博客",
+    subtitle: '椰冻的个人博客',
     description: 'David Ye 的个人技术博客，也许也会有一些生活随笔。本站由 Astro 主题 Retypeset 和 GitHub Pages 强力驱动。',
     posts: '文章',
     tags: '标签',
